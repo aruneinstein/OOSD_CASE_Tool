f86fc2412247a2a1746d0b6a139542bc3958cd72
Danny Cao <ddangcao@gmail.com>
2014-04-17 15:21:12 -0700

    template for project.

--- OOSD_CASE_Tool/C_Obj_Attribute_Editor.Designer.cs
+++ OOSD_CASE_Tool/C_Obj_Attribute_Editor.Designer.cs
@@ -28,33 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(80, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // C_Obj_Attribute_Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
             this.Name = "C_Obj_Attribute_Editor";
-            this.Text = "C_Obj_Attribute_Editor";
+            this.Text = "C Object Attributes Editor";
             this.Load += new System.EventHandler(this.C_Obj_Attribute_Editor_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
