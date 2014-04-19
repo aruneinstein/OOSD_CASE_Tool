c129d1b0462d4d373495921788026b7758792246
Danny Cao <ddangcao@gmail.com>
2014-04-18 22:21:34 -0700

    Added stub for SM-Object form & method.

---
+++ OOSD_CASE_Tool/C_Obj_Attribute_Form.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace OOSD_CASE_Tool
+{
+    partial class C_Obj_Attribute_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // C_Obj_Attribute_Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "C_Obj_Attribute_Editor";
+            this.Text = "C Object Attributes Editor";
+            this.Load += new System.EventHandler(this.C_Obj_Attribute_Editor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ OOSD_CASE_Tool/SM_Obj_Attribute_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOSD_CASE_Tool
+{
+    public partial class SM_Obj_Attribute_Form : Form
+    {
+        public SM_Obj_Attribute_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
