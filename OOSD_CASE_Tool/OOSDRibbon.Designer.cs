15979d274287172f6fc5c91afa0e2cae965427f4
Arun Kumar M S <arun.einstein@gmail.com>
2014-04-27 12:48:39 -0700

    changed stencil. added icons to oosd ribbon

--- OOSD_CASE_Tool/OOSDRibbon.Designer.cs
+++ OOSD_CASE_Tool/OOSDRibbon.Designer.cs
@@ -42,6 +42,9 @@
             this.flowEditorGroup = this.Factory.CreateRibbonGroup();
             this.openFlowStencilBtn = this.Factory.CreateRibbonButton();
             this.convertToArchChartBtn = this.Factory.CreateRibbonButton();
+            this.erToObjHierBtn = this.Factory.CreateRibbonButton();
+            this.objToDictBtn = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
             this.oosdTab.SuspendLayout();
             this.objectEditorGroup.SuspendLayout();
             this.relationEditorGroup.SuspendLayout();
@@ -59,25 +62,32 @@
             // objectEditorGroup
             // 
             this.objectEditorGroup.Items.Add(this.openObjStencilBtn);
+            this.objectEditorGroup.Items.Add(this.objToDictBtn);
             this.objectEditorGroup.Label = "Object Editor";
             this.objectEditorGroup.Name = "objectEditorGroup";
             // 
             // openObjStencilBtn
             // 
+            this.openObjStencilBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.files;
             this.openObjStencilBtn.Label = "Open Stencil";
             this.openObjStencilBtn.Name = "openObjStencilBtn";
+            this.openObjStencilBtn.ShowImage = true;
             this.openObjStencilBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.openObjStencilBtn_Click);
             // 
             // relationEditorGroup
             // 
             this.relationEditorGroup.Items.Add(this.openRelationStencilBtn);
+            this.relationEditorGroup.Items.Add(this.erToObjHierBtn);
+            this.relationEditorGroup.Items.Add(this.button1);
             this.relationEditorGroup.Label = "Relation Editor";
             this.relationEditorGroup.Name = "relationEditorGroup";
             // 
             // openRelationStencilBtn
             // 
+            this.openRelationStencilBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.files;
             this.openRelationStencilBtn.Label = "Open Stencil";
             this.openRelationStencilBtn.Name = "openRelationStencilBtn";
+            this.openRelationStencilBtn.ShowImage = true;
             // 
             // flowEditorGroup
             // 
@@ -88,14 +98,39 @@
             // 
             // openFlowStencilBtn
             // 
+            this.openFlowStencilBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.files;
             this.openFlowStencilBtn.Label = "Open Stencil";
             this.openFlowStencilBtn.Name = "openFlowStencilBtn";
+            this.openFlowStencilBtn.ShowImage = true;
             // 
             // convertToArchChartBtn
             // 
+            this.convertToArchChartBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.play;
             this.convertToArchChartBtn.Label = "Convert to Chart";
             this.convertToArchChartBtn.Name = "convertToArchChartBtn";
+            this.convertToArchChartBtn.ShowImage = true;
             this.convertToArchChartBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.convertToArchChartBtn_Click);
+            // 
+            // erToObjHierBtn
+            // 
+            this.erToObjHierBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.play;
+            this.erToObjHierBtn.Label = "Convert to Object Hierarchy";
+            this.erToObjHierBtn.Name = "erToObjHierBtn";
+            this.erToObjHierBtn.ShowImage = true;
+            // 
+            // objToDictBtn
+            // 
+            this.objToDictBtn.Image = global::OOSD_CASE_Tool.Properties.Resources.play;
+            this.objToDictBtn.Label = "Generate Data Dictionary";
+            this.objToDictBtn.Name = "objToDictBtn";
+            this.objToDictBtn.ShowImage = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::OOSD_CASE_Tool.Properties.Resources.play;
+            this.button1.Label = "Convert to Object Hierarchy";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
             // 
             // OOSDRibbon
             // 
@@ -124,6 +159,9 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup flowEditorGroup;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton openFlowStencilBtn;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton convertToArchChartBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton objToDictBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton erToObjHierBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
     }
 
     partial class ThisRibbonCollection
